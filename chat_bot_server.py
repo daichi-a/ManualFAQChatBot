d6977ed75f12d51b6f376f09aced60b360bef74d
Daichi Ando <daichi_a@mac.com>
2021-09-13 16:29:17 +0900

    CSVの読み込みで、列分を追加

--- chat_bot_server.py
+++ chat_bot_server.py
@@ -159,7 +159,7 @@ def initialize_bert_pre_traind_model():
     df_title_seq = df_csv.loc[:, ['Title0', 'Title1', 'Title2', 'Title3']]
 
     # 特徴量を格納するarrayを作り
-    embedding_array = np.zeros((len(df_title_seq.values), 768), dtype=float)
+    embedding_array = np.zeros((len(df_title_seq.values), 4, 768), dtype=float)
 
     # 特徴量の抽出
     for index, title in np.ndenumerate(df_title_seq.values):
